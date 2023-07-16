f73faf5742b3e5d85f40e49bef3312b21884b9d8
LRomanTomas <tomasroman07@hotmail.com>
2023-07-16 17:04:49 -0300

    Update

--- ejercicios/07_tps/07_FOR_UTN_Factory/app.py
+++ ejercicios/07_tps/07_FOR_UTN_Factory/app.py
@@ -41,8 +41,134 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        
+        contador = 0
 
+        postulantes_ssr = 0
+
+        menor_edad_jr = None
+        nombre_menor_jr = ""
+
+        cantidad_m = 0
+        acumulador_edad_m = 0
+        cantidad_f = 0
+        acumulador_edad_f = 0
+        cantidad_nb = 0
+        acumulador_edad_nb = 0
+
+        tecnologias_elegidas = []
+        cant_python = 0
+        cant_js = 0
+        cant_asp_net = 0
+        tecno_mas_elegida = None
+
+
+        while contador < 3:
+            contador += 1
+            nombre = prompt("UTN","Ingrese el nombre: ")
+
+            edad = int(prompt("UTN","Ingrese la edad: "))
+            while edad < 18:
+                edad = int(prompt("UTN","Ingrese la edad (Debe ser mayor a 17 años): "))
+
+            genero = prompt("UTN","Ingrese el genero: (M, F o NB ")
+            while genero != "M" and genero != "F" and genero != "NB":
+                genero = prompt("UTN","Ingrese el genero: M, F o NB ")
+            else:
+                if genero == "M":
+                    cantidad_m += 1
+                    acumulador_edad_m += edad
+                else:
+                    if genero == "F":
+                        cantidad_f += 1
+                        acumulador_edad_f += edad
+                    else:
+                        cantidad_nb += 1
+                        acumulador_edad_nb+= edad
+            
+            
+            tecnologia = prompt("UTN","Ingrese la tecnologia: ")
+            while tecnologia != "PYTHON" and tecnologia != "JS" and tecnologia != "ASP.NET":
+                tecnologia = prompt("UTN","Ingrese la tecnologia:(PYTHON, JS o ASP.NET) ")
+            
+            tecnologias_elegidas.append(tecnologia)
+
+
+
+            puesto = prompt("UTN","Ingrese el puesto: ")
+            while puesto != "Jr" and puesto != "Ssr" and puesto != "Sr":
+                puesto = prompt("UTN","Ingrese el puesto: (Jr, Ssr o Sr) ")
+
+            if (genero == "NB") and (tecnologia == "JS" or tecnologia == "ASP.NET") and (edad >= 25 and edad <= 40) and (puesto == "Ssr"):
+                postulantes_ssr += 1
+            
+            while puesto == "Jr":
+                if menor_edad_jr == None or edad < menor_edad_jr:
+                    menor_edad_jr = edad
+                    nombre_menor_jr = nombre
+                break
+
+        
+        for tipo in tecnologias_elegidas:
+            if tipo == "PYTHON":
+                cant_python += 1
+            else:
+                if tipo == "ASP.NET":
+                    cant_asp_net += 1
+                else:
+                    cant_js += 1
+
+        
+        if cant_python >= cant_asp_net and cant_python >= cant_js:
+            tecno_mas_elegida = "PYTHON"
+        else:
+            if cant_asp_net >= cant_python and cant_asp_net >= cant_js:
+                tecno_mas_elegida = "ASP.NET"
+            else:
+                tecno_mas_elegida = "JS"
+
+        
+        promedio_m = 0
+        promedio_f = 0
+        promedio_nb = 0
+
+        if cantidad_m != 0:
+            promedio_m = acumulador_edad_m / cantidad_m
+        if cantidad_f != 0:
+            promedio_f = acumulador_edad_f / cantidad_f
+        if cantidad_nb != 0:
+            promedio_nb = acumulador_edad_nb / cantidad_nb
+        
+
+        porcentaje_m = 0
+        porcentaje_f = 0
+        porcentaje_nb = 0
+
+        if cantidad_m != 0:
+            porcentaje_m = cantidad_m * 100 / contador
+
+        if cantidad_f != 0:
+            porcentaje_f = cantidad_f * 100 / contador
+
+        if cantidad_nb != 0:
+            porcentaje_nb = cantidad_nb * 100 / contador
+
+        print(f"""
+                Cantidad de postulantes de genero no binario (NB) que programan en ASP.NET o JS 
+                cuya edad este entre 25 y 40, que se hayan postulado para un puesto Ssr: {postulantes_ssr}
+                ------------------------------------------------------------------------------------------
+                La persona de menor edad postulada para Jr es: {nombre_menor_jr}
+                ------------------------------------------------------------------------------------------
+                Promedio de las edades de las personas de genero Masculino: {promedio_m:.0f}
+                Promedio de las edades de las personas de genero Femenino: {promedio_f:.0f}
+                Promedio de las edades de las personas de genero No binario: {promedio_nb:.0f}
+                ------------------------------------------------------------------------------------------
+                La tecnologia mas elegida fue {tecno_mas_elegida}
+                ------------------------------------------------------------------------------------------
+                El porcentaje de las personas de genero Masculino es : {porcentaje_m}
+                El porcentaje de las personas de genero Femenino es : {porcentaje_f}
+                El porcentaje de las personas de genero No binario es : {porcentaje_nb}""")
+            
 
 if __name__ == "__main__":
     app = App()
