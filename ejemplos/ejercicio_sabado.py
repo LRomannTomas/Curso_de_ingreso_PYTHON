73a15fd5418c6bf3e29dccfb0ac174aad4a6e01d
LRomanTomas <tomasroman07@hotmail.com>
2023-07-15 21:24:48 -0300

    Update

--- ejemplos/ejercicio_sabado.py
+++ ejemplos/ejercicio_sabado.py
@@ -6,6 +6,24 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 import tkinter as tk
 
+# Se deben pedir los siguientes datos de un tour  de vacaciones a un destino en particular:
+
+
+# 1 -nombre , edad y género de una persona, mostrar el mensaje , "usted es  xxxx tiene xx de edad y su género es xxx"
+
+# 2 -pedir la altura de la persona e informar si es bajo: menor a 140 cm,  
+# medio entre 140 y 170 cm , alto hasta 190 cm y muy alto para mayores a esa altura.
+
+# 3- Validar todos los datos.
+
+# 4- En las vacaciones se pueden seleccionar distintas excursiones para realizar. Se pueden hacer desde 0 excursiones a 11 excursiones.
+
+# 5- Una vez ingresada la cantidad se debe pedir por cada excursión 
+# el importe y el tipo de excursión (caminata  o vehículo). 
+# informar cual es el precio más caro, el más barato y el promedio
+
+# 6- Informar cual es el tipo de excursión (caminata  o vehículo) más seleccionada o si se seleccionó las mismas veces (caminata  o vehículo)
+
 class App(customtkinter.CTk):
     
     def __init__(self):
@@ -40,7 +58,7 @@ class App(customtkinter.CTk):
                 edad = int(edad)
                  
 
-        genero = prompt("UTN","Ingrese su genero Masculino, Femenino, o Otro: ")
+        genero = prompt("UTN","Ingrese su genero Masculino, Femenino, u Otro: ")
         while genero != "Masculino" and genero != "Femenino" and genero != "Otro":
             genero = prompt("UTN","Ingrese su genero Masculino, Femenino, u Otro: ")
 
--- ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
+++ ejercicios/07_tps/06_WHILE_Elecciones_Paso/app.py
@@ -29,7 +29,58 @@ class App(customtkinter.CTk):
         self.btn_validar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
 
     def btn_validar_on_click(self):
-        pass
+        x_mas_votado = None
+        nombre_mas_votado = ""
+        x_menos_votado = None
+        nombre_menos_votado = ""
+        bandera = True
+        suma_edades = 0
+        contador = 0
+        acumulador_votos = 0
+        while bandera == True:
+            contador += 1
+            nombre = prompt("UTN","Ingrese su nombre: ")
+            while nombre == None or nombre == "" or nombre.isalpha() == False or len(nombre) < 3:
+                nombre = prompt("UTN","Ingrese un nombre valido: ")
+            
+            edad = int(prompt("UTN","Ingrese la edad: "))
+            while edad < 25:
+                edad = int(prompt("UTN","Ingrese la edad (mayor a 25): "))
+            suma_edades += edad
+
+            votos = int(prompt("UTN","Cantidad de votos recibidos: "))
+            while votos < 0:
+                votos = int(prompt("UTN","Cantidad de votos recibidos (Ingrese un valor valido): "))
+            
+            acumulador_votos += votos
+            while True:
+                if x_mas_votado == None or votos > x_mas_votado:
+                    x_mas_votado = votos
+                    nombre_mas_votado = nombre
+                if x_menos_votado == None or votos < x_menos_votado:
+                    x_menos_votado = votos
+                    nombre_menos_votado = nombre
+                break
+
+            
+            salida = question("Pregunta","Desea continuar?")
+            if salida == False:
+                break
+        
+        promedio_edades = suma_edades / contador
+        print(f"""
+              El candidato mas votado fue: {nombre_mas_votado}
+              El candidato menos votado fue: {nombre_menos_votado}
+              El promedio de las edades de los candidatos es: {promedio_edades}
+              La cantidad de votos totales es: {acumulador_votos}""")
+                
+                
+            
+
+            
+
+
+
     
 
 
